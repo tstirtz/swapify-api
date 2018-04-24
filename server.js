c9b29056f26c68a017edc82c6cbfb9cf0c89ddeb
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-24 10:56:47 -0700

    Install dotev, create PORT env variable.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const {PORT} = require('./config');
 
-app.listen(3000, () => {
-    console.log("Your app is listening on port 3000");
+app.listen(PORT, () => {
+    console.log(`Your app is listening on port ${PORT}`);
 });
