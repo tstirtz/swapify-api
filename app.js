006920448a344cb2e9575fa1314b76235a0d0c58
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-24 19:53:24 -0700

    Set up cors for client url.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const {CLIENT_ORIGIN} = require('./config');
+const cors = require('./cors');
+
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!');
--- config.js
+++ config.js
@@ -1,1 +1,2 @@
 exports.PORT = process.env.PORT || 8000;
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://swapify.netlify.com/';
