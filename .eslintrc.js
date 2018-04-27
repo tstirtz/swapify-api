5caed77b50049a8e1a304a95827baf4bb0da00e6
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-26 23:10:15 -0700

    Add rule for function names.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "rules": {
+      "prefer-promise-reject-errors": ["error", {"allowEmptyReject": true}],
+      "func-names": ["error", "never"]
+    }
 };
