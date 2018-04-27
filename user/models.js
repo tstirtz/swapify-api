6b73b62fe0f31e40ebd5ebbffda5abf1acbf0d01
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-26 23:22:20 -0700

    Add validatePassword and hashPassword schema methods.

--- user/models.js
+++ user/models.js
@@ -1,5 +1,6 @@
 const { DATABASE_URL } = require('../config');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(DATABASE_URL);
@@ -24,6 +25,10 @@ userSchema.methods.serialize = function () {
     username: this.username,
   };
 };
+
+userSchema.methods.validatePassword = password => bcrypt.compare(password, this.password);
+
+userSchema.statics.hashPassword = password => bcrypt.hash(password, 10);
 
 
 const Users = mongoose.model('Users', userSchema);
