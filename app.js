24c1e3068ca5320732d9fe7c68ad9682918476cc
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-26 22:17:17 -0700

    Add morgan and route to user router.js.

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const app = express();
-const {CLIENT_ORIGIN} = require('./config');
-const cors = require('./cors');
 
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
-);
+const app = express();
+const morgan = require('morgan');
+const { CLIENT_ORIGIN } = require('./config');
+const cors = require('cors');
+const usersRouter = require('./user/router');
+
+app.use(morgan('common'));
+app.use(cors({ origin: CLIENT_ORIGIN }));
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
+  res.status(200).send('Hello World!');
 });
+
+app.use('/sign-up', usersRouter);
 
 module.exports = app;
