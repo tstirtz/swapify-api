9622cba88baae78ecd776372810ecacd2ec2a59f
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-28 06:45:57 -0700

    Fix cors import syntax error.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const {CLIENT_ORIGIN} = require('./config');
-const cors = require('./cors');
+const cors = require('cors');
 
 app.use(
     cors({
