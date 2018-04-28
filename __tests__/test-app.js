dfbe492aa117f5bd9a0b46e477abda0b5cfa8b3c
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-28 09:42:32 -0700

    Change file name to test-api.js

---
+++ __tests__/test-app.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe("Root endpoint '/'", () => {
+  test('It should provide response to GET request', () => {
+    return request(app).get('/')
+      .then((res) => {
+        expect(res.status).toEqual(200);
+        expect(res.text).toEqual('Hello World!');
+      });
+  });
+});
