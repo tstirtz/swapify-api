1c5e356a74cbbad996d701e55f318cce60a0c871
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-28 10:27:02 -0700

    Add test for missing field and test returned user object on successful account creation.

--- __tests__/test-usersRouter.js
+++ __tests__/test-usersRouter.js
@@ -10,11 +10,34 @@ describe('/sign-up end point', () => {
       username: 'test123',
       password: 'anothertest',
     };
+    const expectedUser = {
+      name: {
+        first: 'Test',
+        last: 'Test',
+      },
+      email: 'test@test.com',
+      username: 'test123',
+    }
 
     return request(app).post('/sign-up')
       .send(user)
       .then((res) => {
         expect(res.status).toEqual(201);
-      })
+        expect(res.body).toEqual(expectedUser);
+      });
+  });
+  it('Should return error due to missing field', () => {
+    const user = {
+      lastName: 'Test',
+      email: 'test@test.com',
+      username: 'test123',
+      password: 'anothertest',
+    };
+
+    return request(app).post('/sign-up')
+      .send(user)
+      .then((res) => {
+        expect(res.status).toEqual(422);
+      });
   });
 });
