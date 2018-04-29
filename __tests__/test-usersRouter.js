e79ecf384330a33d5baacb8717ec843b5bde0525
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-28 21:22:13 -0700

    Add test for passwords that are too short or too long.

--- __tests__/test-usersRouter.js
+++ __tests__/test-usersRouter.js
@@ -1,7 +1,13 @@
 const request = require('supertest');
 const app = require('../app');
+const { runServer, closeServer } = require('../server');
+const { TEST_DATABASE_URL } = require('../config');
+const { Users } = require('../user/models');
 
 describe('/sign-up end point', () => {
+  beforeAll(() => runServer(TEST_DATABASE_URL));
+  beforeEach(() => Users.remove({}).exec());
+  afterAll(() => closeServer());
   it('Should return new user', () => {
     const user = {
       firstName: 'Test',
@@ -40,4 +46,70 @@ describe('/sign-up end point', () => {
         expect(res.status).toEqual(422);
       });
   });
+
+  it('Should return error for password with leading or trailing whitespace', () => {
+    const user = {
+      firstName: 'Test',
+      lastName: 'Test',
+      email: 'test@test.com',
+      username: 'test123',
+      password: ' anothertest ',
+    };
+    return request(app).post('/sign-up')
+      .send(user)
+      .then((res) => {
+        expect(res.status).toEqual(422);
+        expect(res.body.message).toContain('Cannot start or end with whitespace');
+      });
+  });
+  it('Should return error if password is too long', () => {
+    let tooLargePassword = '';
+    for (let i = 0; i < 73; i += 1) {
+      tooLargePassword += 't';
+    }
+    const user = {
+      firstName: 'Test',
+      lastName: 'Test',
+      email: 'test@test.com',
+      username: 'test123',
+      password: tooLargePassword,
+    };
+    return request(app).post('/sign-up')
+      .send(user)
+      .then((res) => {
+        expect(res.status).toEqual(422);
+        expect(res.body.message).toContain('Password must not be more than 72 characterslong');
+      });
+  });
+  it('Should return error if password is too short', () => {
+    const user = {
+      firstName: 'Test',
+      lastName: 'Test',
+      email: 'test@test.com',
+      username: 'test123',
+      password: 'test',
+    };
+    return request(app).post('/sign-up')
+      .send(user)
+      .then((res) => {
+        expect(res.status).toEqual(422);
+        expect(res.body.message).toContain('Password must be at least 10 characters long');
+      });
+  });
+  // it('Should return error if username is already taken', () => {
+  //   const user = {
+  //     firstName: 'Test',
+  //     lastName: 'Test',
+  //     email: 'test@test.com',
+  //     username: 'test123',
+  //     password: 'anothertest',
+  //   };
+  //
+  //   return request(app).post('/sign-up')
+  //     .send(user)
+  //     .then(() =>
+  //       request(app).post('/sign-up')
+  //         .send(user)
+  //         .then(res => expect(res.status).toEqual(422)))
+  // });
 });
