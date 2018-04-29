5bc9f04f7d11d58e5c8102e194364b5a5f0d97b4
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-28 21:19:37 -0700

    Add runServer and closeServer functions.

--- server.js
+++ server.js
@@ -1,6 +1,48 @@
 const app = require('./app');
 const { PORT } = require('./config');
+const mongoose = require('mongoose');
 
-app.listen(PORT, () => {
-  console.log(`Your app is listening on port ${PORT}`);
-});
+
+let port;
+let server;
+
+function runServer(databaseUrl, port=PORT){
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, (err) => {
+      if(err){
+        return reject(err);
+      }
+      console.log(`mongoose is connected to ${databaseUrl}`);
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+      });
+      resolve();
+    })
+    .catch(err => {
+      mongoose.disconnect();
+      reject(err);
+    })
+  })
+}
+
+function closeServer(){
+  return new Promise((resolve, reject) => {
+    mongoose.disconnect()
+    .then(() => {
+      console.log('Closing server');
+      server.close(err => {
+        if(err){
+          console.log(err);
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err=> console.log(err));
+}
+
+module.exports = { runServer, closeServer };
