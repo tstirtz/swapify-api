b616bba6eb310df9845d141c3b82b946f76e9c9c
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-30 17:10:26 -0700

    Add morganBody middleware.

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const morgan = require('morgan');
 const { CLIENT_ORIGIN } = require('./config');
 const cors = require('cors');
 const usersRouter = require('./user/router');
+const morganBody = require('morgan-body');
 
 app.use(morgan('common'));
-app.use(cors({ origin: CLIENT_ORIGIN }));
+morganBody(app);
+app.use(cors({ origin: [CLIENT_ORIGIN, 'http://localhost:3000'] }));
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
