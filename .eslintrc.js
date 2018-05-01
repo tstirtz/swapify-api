1393d36fe387e394a3cad49d68b1c27d9b41b51b
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-30 17:08:52 -0700

    Add catch-no-return rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "extends": "airbnb-base",
     "rules": {
       "prefer-promise-reject-errors": ["error", {"allowEmptyReject": true}],
-      "func-names": ["error", "never"]
+      "func-names": ["error", "never"],
+      "promise/catch-or-return": "error",
     }
 };
