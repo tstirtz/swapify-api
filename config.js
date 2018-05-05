ae49b4fa1ca348d5a9c041cbee174b8671bc9fd3
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-05 12:34:54 -0700

    Add environment variables for jwt expiration and jwt secret

--- config.js
+++ config.js
@@ -2,3 +2,5 @@ exports.PORT = process.env.PORT || 8000;
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000' || 'https://swapify.netlify.com/';
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/swapify';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-swapify';
+exports.JWT_SECRET = process.env.JWT_SECRET || 'testsecretkey';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
