5f272c044cf34a5180667eec5e5698fc47eebd19
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-05 12:39:50 -0700

    Create login router file and strategies file for user login

---
+++ auth/login-router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const { Users } = require('../user/models');
+
+const config = require('../config');
+
+const router = express.Router();
+
+const localAuth = passport.authenticate('local', { session: false });
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+const createAuthToken = user =>
+  jwt.sign({ user }, config.JWT_SECRET, {
+    subject: user.username,
+    expiresIn: config.JWT_EXPIRY,
+    algorithm: 'HS256',
+  });
+router.post('/', localAuth, (req, res) => {
+  console.log("login router called");
+  const authToken = createAuthToken(req.user.forAuthToken());
+  res.json({ id: req.user._id, jwt: authToken });
+});
+
+module.exports = router;
---
+++ auth/strategies.js
@@ -0,0 +1,48 @@
+const { Strategy: LocalStrategy } = require('passport-local');
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const { Users } = require('../user/models');
+const { JWT_SECRET } = require('../config');
+
+// strategy used for authorization of /login endpoint
+const localStrategy = new LocalStrategy ((username, password, done) => {
+  let user;
+  Users.findOne({ username })
+    .then((_user) => {
+      user = _user;
+      if (!user) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username or password'
+        });
+      }
+      return user.validatePassword(password);
+    })
+    .then((isValid) => {
+      if (!isValid) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username or password',
+        });
+      }
+      return done(null, user);
+    })
+    .catch((err) => {
+      if (err.reason === 'LoginError') {
+        return done(null, false, err);
+      }
+      console.log(err);
+      return done(err, false);
+    });
+});
+
+const jwtStrategy = new JwtStrategy(
+  {
+    secretOrKey: JWT_SECRET,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+    algorithms: ['HS256'],
+  },
+  (payload, done) =>
+    done(null, payload.user),
+);
+
+module.exports = { localStrategy, jwtStrategy };
