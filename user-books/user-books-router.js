f4b6d79d81289f1ccdbd3cac58d44133eeac363a
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-10 20:06:11 -0700

    Create GET route for retrieving a users books

---
+++ user-books/user-books-router.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const router = express.Router({ mergeParams: true });
+const passport = require('passport');
+// const bodyParser = require('body-parser');
+const { BookToSwap } = require('./book-swap-model');
+const { jwtStrategy } = require('../auth/strategies');
+
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+passport.use(jwtStrategy);
+
+router.get('/', jwtAuth, (req, res) => {
+  console.log(req.params.id);
+  return BookToSwap.find({ userId: `${req.params.id}` })
+    .then((books) => {
+      if (books.length === 0) {
+        return res.status(204);
+      }
+      return res.status(200).json(books);
+    })
+    .catch((err) => {
+      console.log(err); res.status(500).json({ message: 'Internal server error. Please try again' })
+    });
+});
+
+module.exports = router;
