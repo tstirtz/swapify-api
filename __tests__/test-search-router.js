84e774ca7470513b90ce69cd250728cd1ac4ab56
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-15 22:12:54 -0700

    Add tests for /search endpoint

---
+++ __tests__/test-search-router.js
@@ -0,0 +1,69 @@
+const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const app = require('../app');
+const { runServer, closeServer } = require('../server');
+const { TEST_DATABASE_URL, JWT_SECRET, JWT_EXPIRY } = require('../config');
+const { BookToSwap } = require('../user-books/book-swap-model');
+const { Users } = require('../user/models');
+
+describe('/search endpoint', () => {
+  let userId;
+  let authToken;
+  beforeAll(() => {
+    runServer(TEST_DATABASE_URL)
+  });
+  beforeAll(() => {
+    const user = {
+      first: 'Test',
+      last: 'Test',
+      email: 'test@test.com',
+      username: 'testUser',
+      password: 'anothertest',
+    };
+    return request(app).post('/sign-up')
+      .send(user)
+      .then((res) => {
+        Users.find({})
+          .then((foundUser) => {
+            console.log(foundUser[0]._id);
+            userId = foundUser[0]._id;
+            console.log(userId);
+          });
+      });
+  });
+  beforeEach(() => {
+    const user = {
+      username: 'test123',
+      _id: userId,
+    };
+    authToken = jwt.sign({ user }, JWT_SECRET, {
+      subject: user.username,
+      expiresIn: JWT_EXPIRY,
+      algorithm: 'HS256',
+    });
+  });
+  beforeEach(() => {
+    const newBook = {
+      userId: 'Test123',
+      title: 'Enders Game',
+      author: 'Orson Scott Card',
+    };
+
+    return BookToSwap.create(newBook);
+  });
+  afterEach(() => {
+    console.log('Deleting db');
+    return BookToSwap.deleteMany();
+  })
+  afterAll(() => {
+    closeServer();
+  });
+  it('Should return all books in the database', () => {
+    return request(app).get('/search')
+      .set('Authorization', `Bearer ${authToken}`)
+      .then((res) => {
+        expect(res.status).toEqual(200);
+        expect(res.body.length).toBeGreaterThan(0);
+      });
+  });
+});
