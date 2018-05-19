2996d9fd469516bfcfc2a4bfa09fb218f8a03eaa
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-18 23:19:24 -0700

    Create post endpoint to create a new Message document

---
+++ messages/message-router.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+const passport = require('passport');
+const { jwtStrategy } = require('../auth/strategies');
+const { Message } = require('./message-model');
+
+router.post('/', (req, res) => {
+  const requiredFields = ['to', 'from', 'content', 'timeStamp'];
+  requiredFields.forEach((field) => {
+    if (!(field in req.body)) {
+      return res.status(422).json({
+        code: 422,
+        reason: 'ValidationError',
+        message: `Missing '${field}' field`,
+      });
+    }
+  });
+  Message.create({
+    to: req.body.to,
+    from: req.body.from,
+    content: req.body.content,
+    timeStamp: req.body.timeStamp,
+  })
+    .then(() => res.status(200).json({ message: 'Message created' }))
+    .catch((err) => {
+      if (err.reason === 'ValidationError') {
+        return res.status(err.code).json(err);
+      }
+      return res.status(500).json({ code: 500, message: 'Internal server error' });
+    });
+});
+
+module.exports = router;
