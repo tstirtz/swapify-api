74e2741dec13776708b035eb2542408a87d9484a
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-21 16:07:32 -0700

    Create router to delete a book from BookToSwap collection

---
+++ user-books/delete-book-router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router({ mergeParams: true });
+
+const passport = require('passport');
+const { jwtStrategy } = require('../auth/strategies');
+const { BookToSwap } = require('./book-swap-model.js');
+
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+passport.use(jwtStrategy);
+
+router.delete('/', (req, res) => {
+  BookToSwap.findOneAndRemove({ _id: `${req.params.bookId}` })
+    .then(response => res.status(200).json(response))
+    .catch(err => res.status(err.code).json(err));
+});
+
+module.exports = router;
