47cd277f1a0bcce26cdf76a202a346fa5cac33a9
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-23 15:24:54 -0700

    Move tests for book-to-swap endpoint to separate file

---
+++ __tests__/test-book-to-swap.js
@@ -0,0 +1,156 @@
+const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const app = require('../app');
+const { runServer, closeServer } = require('../server');
+const { TEST_DATABASE_URL, JWT_SECRET, JWT_EXPIRY } = require('../config');
+const { BookToSwap } = require('../user-books/book-swap-model');
+const { Users } = require('../user/models');
+
+describe('/book-to-swap end point', () => {
+  let userId;
+  let authToken;
+
+  beforeAll(() => runServer(TEST_DATABASE_URL, 4000));
+  beforeEach(() => {
+    const user = {
+      first: 'Test',
+      last: 'Test',
+      email: 'test@test.com',
+      username: 'testUser',
+      password: 'anothertest',
+    };
+    return request(app).post('/sign-up')
+      .send(user)
+      .then((res) => {
+        Users.find({})
+          .then((foundUser) => {
+            console.log(foundUser[0]._id);
+            userId = foundUser[0]._id;
+            console.log(userId);
+            return foundUser;
+          });
+      });
+  });
+
+  beforeEach(() => {
+    const userCredentials = {
+      username: 'testUser',
+      password: 'anothertest',
+    };
+
+    return request(app).post('/login')
+      .send(userCredentials)
+      .then((response) => {
+        console.log(response.body);
+        authToken = response.body.jwt;
+        console.log(authToken);
+      });
+  });
+
+  afterEach(() => {
+    console.log('Deleting books');
+    return BookToSwap.deleteMany();
+  });
+
+  afterEach(() => {
+    console.log('Deleting users');
+    return Users.deleteMany()
+  });
+
+  afterAll(() => closeServer());
+
+  process.on('unhandledRejection', (reason) => {
+    console.error(reason);
+    process.exit(1);
+  });
+
+  test('should return a book', (done) => {
+    console.log(userId);
+    const newBook = {
+      userId: 'testUser',
+      title: 'Enders Game',
+      author: 'Orson Scott Card',
+    };
+    const expectedBook = {
+      title: 'Enders Game',
+      author: 'Orson Scott Card',
+    };
+    // const user = {
+    //   username: 'testUser',
+    //   _id: userId,
+    // };
+    // authToken = jwt.sign({ user }, JWT_SECRET, {
+    //   subject: user.username,
+    //   expiresIn: JWT_EXPIRY,
+    //   algorithm: 'HS256',
+    // });
+
+    return request(app).post('/book-to-swap')
+      .send(newBook)
+      .set('Authorization', `Bearer ${authToken}`)
+      .then((res) => {
+        expect(res.status).toEqual(201);
+        expect(res.body).toContain('Book added successfully');
+        done();
+      });
+  });
+  test('Should return error message if book already exists for a user', (done) => {
+    const newBook = {
+      userId,
+      title: 'Enders Game',
+      author: 'Orson Scott Card',
+    };
+    // const user = {
+    //   username: 'testUser',
+    //   _id: userId,
+    // };
+    // const authToken = jwt.sign({ user }, JWT_SECRET, {
+    //   subject: user.username,
+    //   expiresIn: JWT_EXPIRY,
+    //   algorithm: 'HS256',
+    // });
+    return request(app).post('/book-to-swap')
+      .send(newBook)
+      .set('Authorization', `Bearer ${authToken}`)
+      .then((first) => {
+        console.log(first.body);
+        return request(app).post('/book-to-swap')
+          .send(newBook)
+          .set('Authorization', `Bearer ${authToken}`)
+          .then((res) => {
+            console.log(res.body);
+            expect(res.status).toEqual(422);
+            expect(res.body.message).toContain('Already exists as a');
+            done();
+          });
+      });
+  });
+  test('Should return users books', (done) => {
+    console.log(userId);
+    const newBook = {
+      userId,
+      title: 'Enders Game',
+      author: 'Orson Scott Card',
+    };
+    return request(app).post('/book-to-swap')
+      .send(newBook)
+      .set('Authorization', `Bearer ${authToken}`)
+      .then(() => {
+        return request(app).get(`/user-books/${userId}`)
+          .set('Authorization', `Bearer ${authToken}`)
+          .then((res) => {
+            expect(res.status).toEqual(200);
+            expect(res.body.length).toBeGreaterThanOrEqual(1);
+            done();
+          });
+      });
+  });
+  test('Should return empty array if no books are present for a user', (done) => {
+    return request(app).get(`/user-books/${userId}`)
+      .set('Authorization', `Bearer ${authToken}`)
+      .then((res) => {
+        expect(res.body).toEqual([]);
+        done();
+      });
+  });
+});
