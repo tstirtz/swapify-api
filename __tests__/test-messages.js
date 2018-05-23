fdea6aa5002a2a6942eb7f437a5f02821942f395
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-22 23:34:29 -0700

    Add passing test for missing fields in request body

--- __tests__/test-messages.js
+++ __tests__/test-messages.js
@@ -5,39 +5,100 @@ const { runServer, closeServer } = require('../server');
 const { TEST_DATABASE_URL, JWT_SECRET, JWT_EXPIRY } = require('../config');
 const { BookToSwap } = require('../user-books/book-swap-model');
 const { Users } = require('../user/models');
-const { Message } = require('../messages/send-message-router');
-const { userId } = require('./test-user-books');
-
-let authToken;
-
-beforeAll(() => {
-  const user = {
-    username: 'testUser',
-    _id: userId,
-  };
-  authToken = jwt.sign({ user }, JWT_SECRET, {
-    subject: user.username,
-    expiresIn: JWT_EXPIRY,
-    algorithm: 'HS256',
-  });
-});
+const { Message } = require('../messages/message-model');
 
 describe('/send-message endpoint', () => {
-  console.log(authToken);
-  const newMessage = {
-    to: 'user123',
-    from: 'testUser',
-    content: 'Hey there',
-    timeStamp: '5/22/2018',
-  }
+  let userId;
+  let authToken;
+
+  beforeAll(() => runServer(TEST_DATABASE_URL, 4000));
+  beforeEach(() => {
+    const user = {
+      first: 'Test',
+      last: 'Test',
+      email: 'test@test.com',
+      username: 'testUser',
+      password: 'anothertest',
+    };
+    return request(app).post('/sign-up')
+      .send(user)
+      .then((res) => {
+        Users.find({})
+          .then((foundUser) => {
+            console.log(foundUser[0]._id);
+            userId = foundUser[0]._id;
+            console.log(userId);
+            return foundUser;
+          });
+      });
+  });
+
+  beforeEach(() => {
+    const userCredentials = {
+      username: 'testUser',
+      password: 'anothertest',
+    }
+
+    return request(app).post('/login')
+      .send(userCredentials)
+      .then((response) => {
+        console.log(response.body);
+        authToken = response.body.jwt;
+        console.log(authToken);
+      });
+  });
+
+  afterEach(() => {
+    console.log('Deleting books');
+    return BookToSwap.deleteMany();
+  });
+  afterEach(() => {
+    console.log('Deleting users');
+    return Users.deleteMany();
+  });
+
+  afterEach(() => {
+    console.log('Deleting messages');
+    return Message.deleteMany();
+  });
+
+  afterAll(() => closeServer());
+
+  process.on('unhandledRejection', (reason) => {
+    console.error(reason);
+    process.exit(1);
+  });
 
   it('Should create a new message', (done) => {
+    console.log(authToken);
+    const newMessage = {
+      to: 'user123',
+      from: 'testUser',
+      content: 'Hey there',
+      timeStamp: '5/22/2018',
+    }
+
     return request(app).post('/send-message')
       .send(newMessage)
       .set('Authorization', `Bearer ${authToken}`)
       .then((res) => {
         expect(res.status).toEqual(200);
         expect(res.body).toEqual({ message: 'Message sent' });
+        done();
+      });
+  });
+  it('Should return an error if a field is messing in request body', (done) => {
+    const messageWithMissingField = {
+      from: 'testUser',
+      content: 'Hey there',
+      timeStamp: '5/22/18',
+    };
+
+    return request(app).post('/send-message')
+      .send(messageWithMissingField)
+      .set('Authorization', `Bearer ${authToken}`)
+      .then((res) => {
+        expect(res.status).toEqual(422);
         done();
       });
   });
