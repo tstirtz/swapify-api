0f6d39c79c70a749098a85f3bf71e1ab353178b6
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-23 16:59:47 -0700

    Change promise-reject rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     "extends": "airbnb-base",
     "rules": {
-      "prefer-promise-reject-errors": ["error", {"allowEmptyReject": true}],
+      "prefer-promise-reject-errors": ["warn", {"allowEmptyReject": true}],
       "func-names": ["error", "never"],
       // "promise/catch-or-return": "error",
     }
