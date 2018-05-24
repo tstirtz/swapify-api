b7009e2f27bd1de56ccca3b44fb1ccca6146b82b
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-23 17:05:34 -0700

    Fix syntax errors

--- server.js
+++ server.js
@@ -2,38 +2,40 @@ const mongoose = require('mongoose');
 const { PORT } = require('./config');
 const app = require('./app');
 
-function runServer(databaseUrl, port=PORT){
+let server;
+
+function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
     mongoose.connect(databaseUrl, (err) => {
-      if(err){
+      if (err) {
         return reject(err);
       }
       console.log(`mongoose is connected to ${databaseUrl}`);
       server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
+        console.log(`Your app is listening on port ${port}`);
       });
       resolve();
     })
-    .catch(err => {
-      mongoose.disconnect();
-      reject(err);
-    });
+      .catch((err) => {
+        mongoose.disconnect();
+        reject(err);
+      });
   });
 }
 
-function closeServer(){
+function closeServer() {
   return new Promise((resolve, reject) => {
     mongoose.disconnect()
-    .then(() => {
-      console.log('Closing server');
-      server.close(err => {
-        if(err){
-          console.log(err);
-          return reject(err);
-        }
-        resolve();
+      .then(() => {
+        console.log('Closing server');
+        server.close((err) => {
+          if (err) {
+            console.log(err);
+            return reject(err);
+          }
+          resolve();
+        });
       });
-    });
   });
 }
 
