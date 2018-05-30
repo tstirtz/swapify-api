c73f6b6c51fcbe01fdb1cfe564dc4b09e5bbfbed
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-30 14:25:13 -0700

    Update CLIENT_ORIGIN variable

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.PORT = process.env.PORT || 8000;
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000' || 'https://swapify.netlify.com/';
+exports.CLIENT_ORIGIN = 'https://swapify.netlify.com/';
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/swapify';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-swapify';
 exports.JWT_SECRET = process.env.JWT_SECRET || 'testsecretkey';
