b9ee3b364f4a6cbd3d8370a617e49e2ee8296a08
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-29 20:54:31 -0700

    Clean up code

--- user-books/book-swap-router.js
+++ user-books/book-swap-router.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 const passport = require('passport');
-// const bodyParser = require('body-parser');
 const { BookToSwap } = require('./book-swap-model');
 const { jwtStrategy } = require('../auth/strategies');
 
@@ -11,21 +10,8 @@ const jwtAuth = passport.authenticate('jwt', { session: false });
 passport.use(jwtStrategy);
 
 router.post('/', jwtAuth, (req, res) => {
-  console.log(req.body.userId);
-  console.log(req.body.title);
-  console.log(req.body.author);
-  // const requiredFields = ['title', 'author'];
-  // for (let i = 0; i < requiredFields.length; i += 1) {
-  //   if (!(requiredFields[i] in req.body)) {
-  //     return Promise.reject({
-  //       code: 422,
-  //       message: `${field} is required`,
-  //     });
-  //   }
-  // }
   return BookToSwap.find({ userId: req.body.userId, title: req.body.title })
     .then((item) => {
-      console.log(item);
       if (item.length > 0) {
         return Promise.reject({
           code: 422,
--- user-books/user-books-router.js
+++ user-books/user-books-router.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const router = express.Router({ mergeParams: true });
 const passport = require('passport');
-// const bodyParser = require('body-parser');
 const { BookToSwap } = require('./book-swap-model');
 const { jwtStrategy } = require('../auth/strategies');
 
@@ -11,20 +10,11 @@ const jwtAuth = passport.authenticate('jwt', { session: false });
 passport.use(jwtStrategy);
 
 router.get('/', jwtAuth, (req, res) => {
-  console.log(req.params.id);
   return BookToSwap.find({ userId: `${req.params.id}` })
-    .then((books) => {
-      // if (books.length === 0) {
-      //   return Promise.reject({
-      //     code: 204,
-      //     message: 'No books were found for the user',
-      //   });
-      // }
-      return res.status(200).json(books);
-    })
+    .then(books => res.status(200).json(books))
     .catch((err) => {
       console.log(err);
-      res.status(500).json({ message: 'Internal server error. Please try again' })
+      res.status(500).json({ message: 'Internal server error. Please try again' });
     });
 });
 
